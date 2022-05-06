95b516ce6e65d4921e5c2428d0a21cb768cc5fc7
zereentabassum <islamms262@gmail.com>
2022-05-07 01:02:41 +0600

    environment variable applied

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const cors = require('cors');
+const { MongoClient, ServerApiVersion } = require('mongodb');
+
 require('dotenv').config();
 const port = process.env.PORT || 5000
 
@@ -7,7 +9,19 @@ const app = express();
 
 // middleware
 app.use(cors());
-app.use(express.jsons()); 
+app.use(express.json()); 
+
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.hlqu3.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+client.connect(err => {
+  const collection = client.db("test").collection("devices");
+  console.log('db connected');
+  // perform actions on the collection object
+  client.close();
+});
+
+
 
 app.get('/', (req, res) =>{
     res.send('Running. But I am scared')
