aa7836b6e8a4f47ed3710bcb1a0d692b3485d357
zereentabassum <islamms262@gmail.com>
2022-05-07 13:23:24 +0600

    loaded all items

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 const express = require('express');
 const cors = require('cors');
 const { MongoClient, ServerApiVersion } = require('mongodb');
-
 require('dotenv').config();
 const port = process.env.PORT || 5000
 
@@ -12,16 +11,30 @@ app.use(cors());
 app.use(express.json()); 
 
 
-const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.hlqu3.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+const uri = `mongodb+srv://${process.env.INVENTORY_USER}:${process.env.INVENTORY_PASS}@cluster0.hlqu3.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
-client.connect(err => {
-  const collection = client.db("test").collection("devices");
-  console.log('db connected');
-  // perform actions on the collection object
-  client.close();
-});
 
+async function run(){
+    try{
+        await client.connect();
+        const itemCollection = client.db('inventory').collection('item');
+       
+        app.get('/item', async(req, res) =>{
+          const query = {};
+          const cursor = itemCollection.find(query);
+          const items = await cursor.toArray();
+          res.send(items);
+        })
+    }
+    finally{
 
+    }
+}
+
+run().catch(console.dir);
+
+// inventory
+// L095dFU3Rq8Riio4
 
 app.get('/', (req, res) =>{
     res.send('Running. But I am scared')
